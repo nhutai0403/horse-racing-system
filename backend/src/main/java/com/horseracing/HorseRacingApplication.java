e0b469f7ef6b61a7144d3f12b08d428eeb941287
Jos Nguyễn <phucgammer012345@gmail.com>
2026-05-26 16:26:54 +0700

    chore: update code structure for better readability and maintainability

---
+++ backend/src/main/java/com/horseracing/HorseRacingApplication.java
@@ -0,0 +1,13 @@
+package com.horseracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseRacingApplication.class, args);
+    }
+
+}
